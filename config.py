4fec0794d4134aeb6f3b00fca8bb99264ccedb5a
wenyu song <609923924@qq.com>
2023-08-22 15:29:47 +0800

    修改

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
 	def __init__(self):
-		self.domain = "https://code-run.itechfans.com/"
-		self.proxydomain = "https://code-run.itechfans.com/"
+		self.domain = "https://code-run.itechfans.com"
+		self.proxydomain = "https://code-run.itechfans.com"
 		self.dbname = "Cluster0"
 		self.CORS = False
