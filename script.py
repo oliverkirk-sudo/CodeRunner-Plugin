f98a8d437ef615649ef8ef76f7836b9ed9966b11
wenyu song <609923924@qq.com>
2023-08-24 20:37:29 +0800

    修改

--- script.py
+++ script.py
@@ -530,11 +530,11 @@ async def show_snippet():
         # return the download link
         if snippet_link:
             snippet_link = generate_tinyurl(snippet_link)
-            response = {"snippet_link": snippet_link}
+            response = {"image_link": snippet_link}
             # response['download_png_url'] = generate_tinyurl(download_png_url)
             # response['download_jpg_url'] = generate_tinyurl(download_jpg_url)
             # response['download_svg_url'] = generate_tinyurl(download_svg_url)
-            response['extra_response_instructions'] = f'Output the following markdown tag directly, no more processing is needed. ![Image]({snippet_link})'
+            response['extra_response_instructions'] = f'This is a link to an image that needs to be rendered in markdown format, here are the image tags:\n ![Image]({snippet_link}).\nUse all information in the API response and fully render all Markdown.'
 
         elapsed_time = time.time() - start_time  # calculate the elapsed time
         write_log(f"save_snippet: elapsed time is {elapsed_time} seconds")
